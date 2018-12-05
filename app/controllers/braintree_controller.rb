0036a56b6ecd74befdad2b5592ea24f9eaab06fd
Steven <suzukisteven@gmail.com>
2018-12-05 11:03:39 +0800

    added order summary to checkout page

--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -3,6 +3,7 @@ class BraintreeController < ApplicationController
   def new
     @client_token = Braintree::ClientToken.generate
     @order_item = current_order.order_items.new
+    @order_items = current_order.order_items
   end
 
   def checkout
